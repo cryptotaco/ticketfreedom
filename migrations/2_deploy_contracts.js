bf7fea8507bd55405aa062a834a40c9e015ae11f
Jonathan Wong <jonathanwong@Jonathans-MacBook-Pro.local>
2018-03-31 13:28:53 -0400

    Added EventFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var EventFactory = artifacts.require("EventFactory");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(EventFactory);
 };
