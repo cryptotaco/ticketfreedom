78f8b9d7c300cedf5a3a70993b5ba56d03eb39e1
yihsien <yihsien@princeton.edu>
2018-03-31 16:52:41 -0400

    fix

--- contracts/EventFactory.sol
+++ contracts/EventFactory.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.4.18;
-// import "./Tickets.sol";
+import "./TicketFactory.sol";
 
 contract EventFactory {
 
@@ -16,15 +16,17 @@ contract EventFactory {
     Event[] public events;
 
 
-    event NewEventCreated(address creator, string name, uint eventDate, string location, uint numTickets, uint faceValue);
-    event TicketPurchased(address buyer, uint ticketid, string name, uint eventid);
-    event CreatorIds(uint[] ids);
+    event NewEventCreated(address _creator, string _name, uint _eventDate, string _location, uint _numTickets, uint _faceValue);
+    event TicketPurchased(address _buyer, uint _ticketid, string _name, uint _eventid);
+    event TicketListedForSale(address _seller, uint _ticketId, uint _askPrice);
+    event CreatorIds(uint[] _ids);
+    
     function EventFactory() public {
     }
 
     function createNewEvent(string _name, string _location, uint _eventDate, uint _numTickets, uint _faceValue) public returns (uint) {
-        // address ticketsAddress = new Tickets(_numTickets, _faceValue);
-        uint eventId = events.push(Event(msg.sender, 0, _name, _location, _eventDate, true));
+        address ticketsAddress = new Tickets(msg.sender, _numTickets, _faceValue);
+        uint eventId = events.push(Event(msg.sender, ticketsAddress, _name, _location, _eventDate, true));
         creatorToEventIds[msg.sender].push(eventId);
         
         NewEventCreated(msg.sender, _name, _eventDate, _location, _numTickets, _faceValue);
@@ -37,16 +39,23 @@ contract EventFactory {
     }
 
     function getEventForId(uint _eventId) public view returns ( string name, string location, uint256 eventDate, address tokenAddress ) {
-        Event memory ievent = events[_eventId];
-        return ( ievent.name, ievent.location, ievent.event_date, ievent.tickets_address);
+        Event memory event = events[_eventId];
+        return (event.name, event.location, event.event_date, event.tickets_address);
     }
 
-    function buyTicket(uint _eventId, uint ticketId ) public {
-        Event memory ievent = events[_eventId];
-        //Tickets tf = Tickets(event.token_factory_address);
-        // Try to buy the ticket - is it for sale? 
-        //tf.buyTicket(msg.sender, ticketId);
-        TicketPurchased(msg.sender, ticketId, ievent.name, _eventId);
+    /*
+    function buyTicket(uint _eventId, uint _ticketId ) public {
+        Event memory event = events[_eventId];
+        Tickets tickets = Tickets(event.tickets_address);
+        tickets.buyTicket(_ticketId);
+        TicketPurchased(msg.sender, _ticketId, event.name, _eventId);
     }
+
+    function sellTicket(uint _ticketId, uint _askPrice) public {
+        Tickets tickets = Tickets(event.tickets_address);
+        tickets.sellTicket(_ticketId, _askPrice);
+        TicketListedForSale(msg.sender, _ticketId, _askPrice);
+    }
+    */
 }
 
